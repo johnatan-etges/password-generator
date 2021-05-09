8eb42ee888abf84868d2907f0af3a038a622a2e1
Johnatan Matheus Etges <johnatan.etges@gmail.com>
2021-05-08 21:50:22 -0300

    Appends generated pwd to input. Need changes in future

---
+++ src/GeneratedPasswordView.js
@@ -0,0 +1,9 @@
+const GeneratedPasswordView = {
+
+    render(generatedPassword) {
+            document.getElementById("generated-password").value = generatedPassword
+    }
+
+}
+
+export { GeneratedPasswordView }
