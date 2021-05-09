e77e1448efd3c32c0c5cfd14fe41b0637e32e337
Johnatan Matheus Etges <johnatan.etges@gmail.com>
2021-05-09 20:44:16 -0300

    Module to show and manage pwd options

---
+++ src/GeneratorOptionsView.js
@@ -0,0 +1,48 @@
+const GeneratorOptionsView = {
+    options: [
+        {
+            name: 'Use alphabetic characters',
+            switch_id: 'switch_alphabetic'
+        },
+        {
+            name: 'Use numeric characters',
+            switch_id: 'switch_numeric'
+        },
+        {
+            name: 'Use upper case characters',
+            switch_id: 'switch_uppercase'
+        },
+        {
+            name: 'Use lower case characters',
+            switch_id: 'switch_lowercase'
+        },
+        {
+            name: 'Use simple special characters',
+            switch_id: 'switch_simple_special'
+        },
+        {
+            name: 'Use complex special characters',
+            switch_id: 'switch_complex_special'
+        },
+    ],
+
+    render() {
+        GeneratorOptionsView.options.forEach(option => {
+            document.getElementById("generator-body").innerHTML  = document.getElementById("generator-body").innerHTML +
+            `
+                <div class="generator-option">
+                <div class="generator-option-label">
+                    <span>${option.name}</span>
+                    <span class="tip"><img src="./assets/interrogation.png"/></span>
+                </div>
+                <label class="switch">
+                    <input id="${option.switch_id}" type="checkbox"/>
+                    <span class="slider"></span>
+                </label>
+                </div>
+            `
+        })
+    }
+}
+
+export { GeneratorOptionsView }
