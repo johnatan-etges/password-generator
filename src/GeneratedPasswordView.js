b7d30b9885ab44c3ff3bf9dc0df4a955ecbbea6a
Johnatan Matheus Etges <johnatan.etges@gmail.com>
2021-05-09 19:14:15 -0300

    Input gen brought all to View Module

--- src/GeneratedPasswordView.js
+++ src/GeneratedPasswordView.js
@@ -1,7 +1,15 @@
 const GeneratedPasswordView = {
 
     render(generatedPassword) {
-            document.getElementById("generated-password").value = generatedPassword
+            document.getElementById("generator-header").innerHTML =
+            `
+                <input type="password" id="generated-password" class="generated-password" value="${generatedPassword}"/>
+                <div class="generated-password-options">
+                <span id="reveal-generated-password" class="reveal-generated-password-show" onClick="handleShowHidePassWord(this.id, this.nextElementSibling.id, 'text')"><img src="./assets/show-pwd.png"/></span> <!-- reveal-generated-password-hide -->
+                <span id="blur-generated-password" class="blur-generated-password-hide" onClick="handleShowHidePassWord(this.id, this.previousElementSibling.id, 'password')"><img src="./assets/hide-pwd.png"/></span> <!-- blur-generated-password-show -->
+                <span id="copy-generated-password" class="copy-generated-password" onClick="handleCopyPasswordClick()"><img src="./assets/copy.png"/></span>
+            </div>
+            `
     }
 
 }
